9224449cf28ac20c6f6588d21092b20068f09f6e
이승준 <terryjohnson@terry.local>
2018-05-12 21:57:12 +0900

    로그인 전 메인 페이지 중간 완성본

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var app = express();
 
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({extended:false}));
+
 var jade = require('jade');
 
 var index = require('./routers/index.js');
--- routers/index.js
+++ routers/index.js
@@ -3,6 +3,10 @@ var router = express.Router();
 
 router.get('/',function(req,res,next){
     res.render('index/index');
-})
+});
+
+router.post('/login',function(req,res,nex){
+    console.log("user id : "+req.body.uid);
+});
 
 module.exports = router;
